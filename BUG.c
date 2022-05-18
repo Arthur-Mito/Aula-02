cf9ac5c681370a2bcb9b38d4a5be49379344271d
ARTHUR DE JESUS SCHLINDWEIN <arthur_schlindwein@estudante.sesisenai.org.br>
2022-05-17 21:50:16 -0300

    Diversão

--- BUG.c
+++ BUG.c
@@ -1,1 +1,17 @@
-BUGBUG
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    SetConsoleOutputCP((UINT)65001);
+
+    char sn;
+
+    printf("\nSEU COMPUTADOR ESTÁ BUGADO, REINICIAR AGORA(s/n)?\n");
+    scanf("%s",&sn);
+
+    printf("\n... REINICIANDO ...\n");
+ 
+ 
+
+    return(0);
+}
--- ERRO.c
+++ ERRO.c
@@ -1,1 +1,15 @@
-ERROERRO
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    SetConsoleOutputCP((UINT)65001);
+
+    printf("\n... ERRO CRÍTICO ...\n");
+    
+
+    printf("\n... REINICIANDO ...\n");
+ 
+ 
+
+    return(0);
+}
